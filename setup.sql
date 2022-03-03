525e80dbb7697a20655766c0b8cb72f300167fc2
chathamabate <chathamabate@gmail.com>
2022-03-02 18:24:13 -0600

    First draft of python script done!!!

--- setup.sql
+++ setup.sql
@@ -6,7 +6,8 @@ CREATE TABLE "chathamabate/jdg"."players" (
     lastname VARCHAR(255),
     college VARCHAR(255),
     birthday DATE,
-    PRIMARY KEY (pid)
+    PRIMARY KEY (pid),
+    CONSTRAINT positive_pid CHECK(pid >= 0)
 );
 
 -- Insert Root User (i.e. ME!)
@@ -14,10 +15,10 @@ INSERT INTO "chathamabate/jdg"."players" (pid, firstname, lastname, college, bir
 VALUES (0, 'Chatham', 'Abate', 'McMurtry', '04-01-1999');
 
 -- Insert Non Root User (i.e. Someone else)
-INSERT INTO "chathamabate/jdg"."players" (pid, firstname, lastname, college, birthday)
-VALUES 
-    ((SELECT MAX(pid) + 1 FROM "chathamabate/jdg"."players"),
-    'Anastasia', 'Alexander', 'McMurtry', '01-07-1999');
+-- INSERT INTO "chathamabate/jdg"."players" (pid, firstname, lastname, college, birthday)
+-- VALUES 
+--     ((SELECT MAX(pid) + 1 FROM "chathamabate/jdg"."players"),
+--     'Anastasia', 'Alexander', 'McMurtry', '01-07-1999');
 
 
 
@@ -27,17 +28,18 @@ CREATE TABLE "chathamabate/jdg"."games" (
     league_match BOOLEAN DEFAULT FALSE,
     game_start TIMESTAMP,
     loc VARCHAR(255),
-    PRIMARY KEY (gid)
+    PRIMARY KEY (gid),
+    CONSTRAINT positive_gid CHECK(gid >= 0)
 );
 
 -- Insert First Game.
-INSERT INTO "chathamabate/jdg"."games" (gid, league_match, game_start, loc)
-VALUES (0, TRUE, '3-23-2022 10:00 PM', 'Jones');
+-- INSERT INTO "chathamabate/jdg"."games" (gid, league_match, game_start, loc)
+-- VALUES (0, TRUE, '3-23-2022 10:00 PM', 'Jones');
 
 -- Insert Arbitrary League Match.
-INSERT INTO "chathamabate/jdg"."games" (gid, league_match, game_start, loc)
-VALUES 
-    ((SELECT MAX(gid) + 1 FROM "chathamabate/jdg"."games"), TRUE, '3-23-2022 10:00 PM', 'Jones');
+-- INSERT INTO "chathamabate/jdg"."games" (gid, league_match, game_start, loc)
+-- VALUES 
+--     ((SELECT MAX(gid) + 1 FROM "chathamabate/jdg"."games"), TRUE, '3-23-2022 10:00 PM', 'Jones');
 
 
 
@@ -45,46 +47,46 @@ VALUES
 -- at what positions.
 CREATE TABLE "chathamabate/jdg"."rosters" (
     gid INTEGER,
-    pid INTEGER,
     pos INTEGER,
-    PRIMARY KEY(gid, pid, pos),
+    pid INTEGER,
+    PRIMARY KEY(gid, pos),
 
     CONSTRAINT game_exists
         FOREIGN KEY(gid) REFERENCES "chathamabate/jdg"."games"(gid),
-    CONSTRAINT player_exists
-        FOREIGN KEY(pid) REFERENCES "chathamabate/jdg"."players"(pid),
     CONSTRAINT pos_is_valid
-        CHECK(0 <= pos AND pos <= 3)
-);
-
--- Insert First game rosters into rosters table.
-INSERT INTO "chathamabate/jdg"."rosters" (gid, pid, pos)
-VALUES  (0, 1, 0),
-        (0, 4, 1),
-        (0, 7, 2),
-        (0, 3, 3);
-        
+        CHECK(0 <= pos AND pos <= 3),
+    CONSTRAINT player_exists
+        FOREIGN KEY(pid) REFERENCES "chathamabate/jdg"."players"(pid)
+);      
 
 
 -- Turns Table
 CREATE TABLE "chathamabate/jdg"."turns" (
     gid INTEGER,
-    pid INTEGER,
+    pos INTEGER,
     round_num INTEGER,
     bet INTEGER NOT NULL,
     earned INTEGER NOT NULL,
-    PRIMARY KEY(gid, pid, round_num),
+    PRIMARY KEY(gid, pos, round_num),
 
-    CONSTRAINT game_exists
-        FOREIGN KEY(gid) REFERENCES "chathamabate/jdg"."games"(gid),
-    CONSTRAINT player_exists
-        FOREIGN KEY(pid) REFERENCES "chathamabate/jdg"."players"(pid),
-    CONSTRAINT valid_turn
-        CHECK(
-            (1 <= round_num AND round_num <= 25) AND
-            (0 <= bet AND bet <= round_num)
-        ) 
+    CONSTRAINT game_and_turn_exist
+        FOREIGN KEY(gid, pos) REFERENCES "chathamabate/jdg"."rosters"(gid, pos),
+    CONSTRAINT valid_round
+        CHECK(1 <= round_num AND round_num <= 25)
+
+    -- Checks on bet and earned columns will occur through the use of a view... 
 );
+
+-- Table Drop Commands...
+DROP TABLE "chathamabate/jdg"."turns";
+DROP TABLE "chathamabate/jdg"."rosters";
+DROP TABLE "chathamabate/jdg"."games";
+DROP TABLE "chathamabate/jdg"."players";
+
+-- Table Delete Commands...
+DELETE FROM "chathamabate/jdg"."turns" WHERE 1=1;
+DELETE FROM "chathamabate/jdg"."rosters" WHERE 1=1;
+DELETE FROM "chathamabate/jdg"."games" WHERE 1=1;
 
 
 
--- upload.py
+++ upload.py
@@ -1,13 +1,13 @@
-from cmath import e
-from distutils.log import error
 import os
+import shutil
 import json
 import datetime as dt
+from collections import defaultdict
 
-# This script will parse game csv files in the form :
-# MM.DD.YY.HH:MM(AM|PM).csv
-#
-# Each file of this form found in the "new_data" directory 
+import bitdotio 
+from psycopg2.extras import execute_values
+
+# Each file of found in the "new_data" directory 
 # will be uploaded and then moved into the "old_data directory".
 #
 # creds.json is a file which is required and gives the credentials
@@ -22,6 +22,12 @@ OLD_DATA_PATH = os.path.join(DIR_PATH, "old_data")
 NEW_DATA_PATH = os.path.join(DIR_PATH, "new_data")
 CREDS_PATH = os.path.join(DIR_PATH, "creds.json")
 
+# Fields which must be present in creds.json.
+REQ_CREDS_FIELDS = [
+    "api_key",
+    "players", "games", "rosters", "turns"
+]
+
 # I am assuming these players are static.
 # i.e. they will not be changing throughout the course of the league.
 PLAYER_IDS = {
@@ -35,19 +41,133 @@ PLAYER_IDS = {
     "Alyson" : 7
 }
 
-# Given a csv from the senior league sheet...
-# Add three header lines. The first line must specify the datetime
-# in the following format...
 DATE_PATTERN = "%m/%d/%Y %I:%M %p"
 
-def ind_to_cards(ind):
-    return ind if ind <= 13 else 26 - ind
 
-# The second line must be in the form
-# (true|false)
-# The true or false value specifies whether the match is a league match
-# or not... 
-# The third line specifies the location.
+# Function for processing the turns data of a game.
+def process_turns(data_lines):
+    results = [[pos, round_num, -1, -1] 
+                for round_num in range(1, 26)
+                for pos in range(4)]
+
+    error_lines = defaultdict(list)
+
+    # The next 25 will be read as turn lines.
+    for round_ind in range(1, 26):
+        expected_card_total = round_ind if round_ind <= 13 else 26 - round_ind
+
+        line = data_lines[round_ind - 1]
+        args = line.split(",")
+        # First get the results... then get the
+        try:
+            card_total = int(args[0])
+
+            # Confirm card amount is correct for given round.
+            if card_total != expected_card_total:
+                error_lines[round_ind].append(f"Invalid card amount ({card_total})")
+                continue
+            
+            bet_total = 0
+            earned_total = 0
+
+            for i in range(4):
+                bet = int(args[4 + i*3])
+                if bet < 0 or expected_card_total < bet:
+                    error_lines[round_ind].append(
+                        f"Invalid bet (Player {i}, Bet {bet})"
+                    )
+
+                earned = int(args[5 + i*3])
+                if earned < 0 or expected_card_total < earned:
+                    error_lines[round_ind].append(
+                        f"Invalid earned (Player {i}, Earned {earned})"
+                    )
+
+                bet_total += bet
+                earned_total += earned
+
+                ind = (round_ind - 1)*4 + i
+
+                results[ind][2] = bet
+                results[ind][3] = earned
+            
+            if bet_total == card_total:
+                error_lines[round_ind].append(f"Invalid betting total ({bet_total})")
+            
+            if earned_total != card_total:
+                error_lines[round_ind].append(f"Invalid earned total ({earned_total})")
+
+        except:
+            error_lines[round_ind].append("Error while parsing line")
+
+    return results, error_lines
+
+
+def construct_new_game(curr, games_table, game_start, league_match, loc):
+    # First, get the maximum id in use currently.
+    max_query = f"""
+    SELECT 
+        CASE 
+            WHEN COUNT(*) = 0 THEN 0 
+            ELSE MAX(gid) + 1 
+        END 
+    FROM {games_table};"""
+
+    gid = -1
+
+    try:
+        curr.execute(max_query)
+        top_row = curr.fetchone()
+        gid = top_row[0]
+    except:
+         return -1, "Could not fetch max gid"
+
+    insert_query = f"""
+    INSERT INTO {games_table} (gid, league_match, game_start, loc)
+    VALUES ({gid}, {league_match}, \'{game_start}\', \'{loc}\');
+    """
+
+    try:
+        curr.execute(insert_query)
+    except:
+        return -1, "Could not insert new game"
+
+    return gid, None
+
+
+def insert_rosters(curr, rosters_table, gid, pids):
+    if len(pids) != 4:
+        return "Incorrect number of players given"
+
+    insert_query = f"""
+    INSERT INTO {rosters_table} (gid, pos, pid)
+    VALUES %s;
+    """
+
+    rows = [(gid, pos, pids[pos]) for pos in range(4)]
+
+    try:
+        execute_values(curr, insert_query, rows)
+    except:
+        return "Could not insert roster"
+    
+    return None
+
+
+def insert_turns(curr, turns_table, gid, turns_data):
+    insert_query = f"""
+    INSERT INTO {turns_table} (gid, pos, round_num, bet, earned)
+    VALUES %s;
+    """
+    rows = [(gid, t[0], t[1], t[2], t[3]) for t in turns_data]
+
+    try:
+        execute_values(curr, insert_query, rows)
+    except:
+        return "Could not insert turns"
+    
+    return None
+
 
 creds = None
 
@@ -56,8 +176,25 @@ try:
     creds_f = open(CREDS_PATH, "r")
     creds = json.load(creds_f)
     creds_f.close()
-except e:
-    print("Cannot located creds.json")
+except:
+    print("Cannot locate creds.json")
+    exit(1)
+
+missing_creds = [field  for field in REQ_CREDS_FIELDS 
+                        if field not in creds]
+
+# Verify credentials.
+if len(missing_creds) > 0:
+    print(f"Fields missing in creds.json {missing_creds}")
+    exit(1)
+
+curr = None
+try:
+    b = bitdotio.bitdotio(creds["api_key"])
+    conn = b.get_connection()
+    curr = conn.cursor()
+except:
+    print("Unable to construct cursor into database")
     exit(1)
 
 # All new_data files to check.
@@ -65,77 +202,74 @@ new_data_files = os.listdir(NEW_DATA_PATH)
 
 for df in new_data_files:
     print(f"> {df}")
+    lines = []
 
-    match_dt = None
-    league_match = False
-    location = ""
-    results = [[{"bet": -1, "earned": -1} for _ in range(4)] 
-                for _ in range(1, 26)]
-    pids = [-1, -1, -1, -1]
+    abs_path = os.path.join(NEW_DATA_PATH, df)
 
-    # First attempt to extract necessary information from 
-    # the file.
-    with open(os.path.join(NEW_DATA_PATH, df), "r") as f:
+    # This file should always exist. (extract its lines)
+    with open(abs_path, "r") as f:
         lines = [line[:-1] for line in f.readlines()]
 
-        # The file should have no less than 30 lines.
-        if len(lines) < 30:
-            print(f"  Too few lines in file ({len(lines)})")
-            continue
+    # The file should have no less than 30 lines.
+    if len(lines) < 30:
+        print(f"  Too few lines in file ({len(lines)})")
+        continue
+    
+    match_dt = None
+    try:
+        match_dt = dt.datetime.strptime(lines[0], DATE_PATTERN)
+    except:
+        print(f"  Bad date header ({lines[0]})")
+        continue
 
-        try:
-            match_dt = dt.datetime.strptime(lines[0], DATE_PATTERN)
-        except:
-            print(f"  Bad date header ({lines[0]})")
-            continue
+    league_match = lines[1].startswith("true")
+    location = lines[2]
 
-        league_match = lines[1].startswith("true")
-        location = lines[2]
-
-        try:
-            names = lines[4].split(",")
-            for i in range(4):
-                name = names[i*3 + 3]
-                pids[i] = PLAYER_IDS[name]
-        except:
-            print("Error parsing player names header")
-            continue
-
-        # Toss first five lines now that header has been processed.
-        lines = lines[5:]
-        error_lines = {}
-
-        # The next 25 will be read as turn lines.
-        for round_ind in range(1, 26):
-            line = lines[round_ind - 1]
-            args = line.split(",")
-            # First get the results... then get the
-            try:
-                card_amount = args[0]
-
-                # Confirm card amount is correct for given round.
-                if int(card_amount) != ind_to_cards(round_ind):
-                    error_lines[round_ind] = f"Invalid card amount ({card_amount})"
-                    continue
-
-                for i in range(4):
-                    results[round_ind - 1][i]["bet"] = int(args[4 + i*3])
-                    results[round_ind - 1][i]["earned"] = int(args[5 + i*3])
-            except:
-                error_lines[round_ind] = f"Error while parsing line"
-        
-        if len(error_lines) != 0:
-            for round_ind, error_str in error_lines.items():
-                print(f"  [{round_ind}] {error_str}")
-            continue 
-
-    # Done with the file itself.
-    # Time to run SQL.
-    # First, create the game, then the roster, then the
-    for r in range(1, 26):
-        print(f"Round {r}")
+    pids = [-1, -1, -1, -1]
+    try:
+        names = lines[4].split(",")
         for i in range(4):
-            print(f"Player {pids[i]} : {results[r - 1][i]}")
+            name = names[i*3 + 3]
+            pids[i] = PLAYER_IDS[name]
+    except:
+        print("  Error parsing player names header")
+        continue
+
+    # Toss first five lines now that header has been processed.
+    # Time to parse the actual turn data.
+    lines = lines[5:]
+    
+    results, error_lines = process_turns(lines)
+    
+    if len(error_lines) != 0:
+        for round_ind, errors in error_lines.items():
+            print(f"  [{round_ind}] Round errors")
+            for err in errors:
+                print(f"  * {err}")
+        continue 
+    
+    gid, error = construct_new_game(curr, creds["games"], match_dt, 
+                                    league_match, location)
+
+    if error != None:
+        print(f"  SQL Error, {error}")
+        continue
+
+    error = insert_rosters(curr, creds["rosters"], gid, pids)
+
+    if error != None:
+        print(f"  SQL Error, {error}")
+        continue
+    
+    error = insert_turns(curr, creds["turns"], gid, results)
+
+    if error != None:
+        print(f"  SQL Error, {error}")
+        continue
+
+    # Finally, move this file into the old datafile location.
+    shutil.move(abs_path, os.path.join(OLD_DATA_PATH, df))
+
             
 
 
