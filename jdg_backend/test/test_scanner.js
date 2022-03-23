87c6b72e3e83c56d0a30d2ec0f8d86437f2165a6
chathamabate <chathamabate@gmail.com>
2022-03-22 19:15:21 -0500

    Scanner Failure Tests

--- jdg_backend/test/test_scanner.js
+++ jdg_backend/test/test_scanner.js
@@ -46,17 +46,57 @@ const et4 = [
 ];
 
 const et5 = [
-    "case -->  <== ()]*",
+    "case --> \n\n  <== ()]*",
     [TokenType.CAS, TokenType.MIN, TokenType.ARR, TokenType.LTE, TokenType.EQU,
      TokenType.LPN, TokenType.RPN, TokenType.RBR, TokenType.TIM, TokenType.EOF]
-]
+];
+
+const et6 = [
+    "\"Hello 1 2 3\" 00 \"\"",
+    [TokenType.STV, TokenType.NMV, TokenType.NMV, 
+     TokenType.STV, TokenType.EOF]
+];
+
+const et7 = [
+    "hello123 123hello",
+    [TokenType.VID, TokenType.NMV, TokenType.VID, TokenType.EOF]
+];
 
 const ets = [
-    et1, et2, et3, et4, et5
+    et1, et2, et3, et4, et5, et6, et7
 ];
 
 describe("Expected Tests", () => {
     for (let i = 0; i < ets.length; i++) {
         it("ET " + (i + 1), expectTokenTypes(ets[i]));
+    }
+});
+
+function expectFailure(data) {
+    return () => {
+        let sc = new JQLScanner(data);
+
+        // We expect this will never return the EOF token.
+
+        while (true) {
+            let [t, err] = sc.next();
+            if (err != null) {
+                return;
+            }
+
+            if (t.token_type == TokenType.EOF) {
+                throw new Error("Data successfully parsed.")
+            }
+        }
+    };
+}
+
+const efs = [
+    "1.", ".1", "\"hello World00", "\"\"\"", "$$$"
+];
+
+describe("Failure Tests", () => {
+    for (let i =  0; i < efs.length; i++) {
+        it("FT " + (i + 1), expectFailure(efs[i]));
     }
 });
